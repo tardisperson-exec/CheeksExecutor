1cdeaa8e44ddd9b56367a560b8c5ad7fb9262049
User <User@KYLE-LAPTOP>
2025-11-05 19:09:34 +0200

    Add project files.

---
+++ CheeksExec/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CheeksExec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AttachBTN = new System.Windows.Forms.Button();
+            this.InfYeildBTN = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.quorumMonaco11 = new QuorumMonaco.QuorumMonaco1();
+            this.ExecuteBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AttachBTN
+            // 
+            this.AttachBTN.Location = new System.Drawing.Point(534, 13);
+            this.AttachBTN.Name = "AttachBTN";
+            this.AttachBTN.Size = new System.Drawing.Size(145, 23);
+            this.AttachBTN.TabIndex = 3;
+            this.AttachBTN.Text = "Inject";
+            this.AttachBTN.UseVisualStyleBackColor = true;
+            this.AttachBTN.Click += new System.EventHandler(this.AttachBTN_Click);
+            // 
+            // InfYeildBTN
+            // 
+            this.InfYeildBTN.Location = new System.Drawing.Point(534, 151);
+            this.InfYeildBTN.Name = "InfYeildBTN";
+            this.InfYeildBTN.Size = new System.Drawing.Size(66, 45);
+            this.InfYeildBTN.TabIndex = 5;
+            this.InfYeildBTN.Text = "IY";
+            this.InfYeildBTN.UseVisualStyleBackColor = true;
+            this.InfYeildBTN.Click += new System.EventHandler(this.InfYeildBTN_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 240);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Is Attached?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // quorumMonaco11
+            // 
+            this.quorumMonaco11.Location = new System.Drawing.Point(13, 13);
+            this.quorumMonaco11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.quorumMonaco11.Name = "quorumMonaco11";
+            this.quorumMonaco11.Size = new System.Drawing.Size(514, 428);
+            this.quorumMonaco11.TabIndex = 7;
+            // 
+            // ExecuteBTN
+            // 
+            this.ExecuteBTN.Location = new System.Drawing.Point(534, 66);
+            this.ExecuteBTN.Name = "ExecuteBTN";
+            this.ExecuteBTN.Size = new System.Drawing.Size(145, 79);
+            this.ExecuteBTN.TabIndex = 8;
+            this.ExecuteBTN.Text = "Execute";
+            this.ExecuteBTN.UseVisualStyleBackColor = true;
+            this.ExecuteBTN.Click += new System.EventHandler(this.ExecuteBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 454);
+            this.Controls.Add(this.ExecuteBTN);
+            this.Controls.Add(this.quorumMonaco11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InfYeildBTN);
+            this.Controls.Add(this.AttachBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AttachBTN;
+        private System.Windows.Forms.Button InfYeildBTN;
+        private System.Windows.Forms.Button button1;
+        private QuorumMonaco.QuorumMonaco1 quorumMonaco11;
+        private System.Windows.Forms.Button ExecuteBTN;
+    }
+}
+
---
+++ CheeksExec/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuorumAPI;
+
+namespace CheeksExec
+{
+    public partial class Form1 : Form
+    {
+        private QuorumModule quorum;
+        public Form1()
+        {
+            InitializeComponent();
+            quorum = new QuorumModule();
+            quorum.StartCommunication();
+        }
+
+        private void AttachBTN_Click(object sender, EventArgs e)
+        {
+            quorum.AttachAPI();
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            quorum.StopCommunication();
+        }
+
+        private void InfYeildBTN_Click(object sender, EventArgs e)
+        {
+            quorum.Execute("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isAttached = quorum.IsAttached();
+        }
+
+        private async void ExecuteBTN_Click(object sender, EventArgs e)
+        {
+            string script = await QuorumMonaco.CoreFunctions.GetText();
+            quorum.ExecuteScript(script);
+        }
+    }
+}
